446676c35daa2023347d41630a966b5a563c6669
TheCodeHeist <58696699+TheCodeHeist@users.noreply.github.com>
2021-09-18 17:12:52 +0600

    Update cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,39 @@
-export function cli(args) {
+const inquirer = require("inquirer");
+inquirer
+  .prompt([
+    {
+      type: "list",
+      name: "action",
+      message: "What do you want to do?",
+      choices: [
+        {
+          name: "See Operating System Details",
+          value: "os",
+        },
+        {
+          name: "See CPU Usage",
+          value: "cpu",
+        },
+        {
+          name: "See Memory Usage",
+          value: "memory",
+        },
+        {
+          name: "See Disk Usage",
+          value: "disk",
+        },
+        {
+          name: "See Network Usage",
+          value: "network",
+        },
+      ],
+      default: "os",
+    },
+  ])
+  .then(function (answers) {
+    console.log(answers);
+  });
+
+module.exports = function cli(args) {
   console.log(args);
 }
